ee68296c6ae374d064e9c1f1d29699b67d836127
Daniel Casper <dc@danielcasper.com>
2026-05-03 11:44:04 -0500

    Delete tests/web/hero.test.tsx
    again goodbye

--- tests/web/hero.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Hero from '../../src/web/components/Hero';
-
-test('renders the Hero component with correct content', () => {
-  render(<Hero />);
-  const taglineElement = screen.getByText(/Build software, not debt./i);
-  expect(taglineElement).toBeInTheDocument();
-});
